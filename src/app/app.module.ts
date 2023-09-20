24a4d45ee3c7d543fbae92e9ae9c29ada4e1537d
Gabriel Thomas <gabriel.alphon@gmail.com>
2023-09-20 17:43:02 -0300

    setup environment + bootstrap card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule
