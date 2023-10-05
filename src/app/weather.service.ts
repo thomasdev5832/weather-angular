40152d9d0f6ad4cfffc06d8ee73daee26e998bf7
Gabriel Thomas <gabriel.alphon@gmail.com>
2023-10-05 15:03:09 -0300

    add environments

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,14 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
+  API_KEY: string = environment.API_KEY;
+
   constructor(private http: HttpClient) { }
 
   getWeather(city: string, units: string) {
-    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid=cf579adcb264c37964ef2339d8abc955&units='+ units);
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid='+this.API_KEY+'&units='+ units);
   }
 }
